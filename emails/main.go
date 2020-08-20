0ce7f1066687e3878eb2caf458d98f703ce65c53
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2020-08-20 12:05:05 -0400

    Update main.go

--- emails/main.go
+++ emails/main.go
@@ -11,30 +11,9 @@ import (
 	"time"
 )
 
-/* Tasks
-- 1. Download all the list
-- 2. Combine all the list into one.
-- 3. Check and valid if the domain has a NS or a MX record;
-- 4. If the domain has a NS or a MX record than its a valid domain and goes to the output.
-- 5. Add checks to make sure its not added twice.
-- 6. Remove all the original files; don't save anything other than a standard output.
-*/
-
-/*
-"https://gist.githubusercontent.com/adamloving/4401361/raw/66688cf8ad890433b917f3230f44489aa90b03b7"
-"https://gist.githubusercontent.com/michenriksen/8710649/raw/d42c080d62279b793f211f0caaffb22f1c980912"
-"https://raw.githubusercontent.com/wesbos/burner-email-providers/master/emails.txt"
-"https://raw.githubusercontent.com/andreis/disposable/master/blacklist.txt"
-"https://raw.githubusercontent.com/GeroldSetz/emailondeck.com-domains/master/emailondeck.com_domains_from_bdea.cc.txt"
-"https://raw.githubusercontent.com/andreis/disposable/master/whitelist.txt"
-"https://raw.githubusercontent.com/andreis/disposable-email-domains/master/domains.txt"
-"https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/wildcard.json"
-"https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/index.json"
-*/
-
 const (
-	DownloadWorkers = 8
-	ProcessWorkers  = 4
+	DownloadWorkers = 50
+	ProcessWorkers  = 100
 
 	FileOutputName = "output.txt"
 )
@@ -312,5 +291,17 @@ var urls = []URLType{
 	{
 		URL:  "https://raw.githubusercontent.com/ivolo/disposable-email-domains/master/index.json",
 		Type: "json",
+	},
+	{	
+		URL:  "https://raw.githubusercontent.com/di/disposable-email-domains/master/source_data/disposable_email_blocklist.conf",	
+		Type: "txt",	
+	},	
+	{	
+		URL:  "https://raw.githubusercontent.com/willwhite/freemail/master/data/disposable.txt",	
+		Type: "txt",	
+	},	
+	{	
+		URL:  "https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.txt",	
+		Type: "txt",	
 	},
 }
